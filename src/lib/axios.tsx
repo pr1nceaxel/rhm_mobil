42d888f203b0016caf310e166abd7bc3b5c4eb6f
pr1nceaxel <axelvianney49@gmail.com>
2024-09-04 15:18:16 +0000

    localhost

--- src/lib/axios.tsx
+++ src/lib/axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-
-const E_RHM_API_URL = "http://172.20.10.3:5000/api";
+const LOCALHOST = '192.168.1.11'
+const E_RHM_API_URL = `http://${LOCALHOST}:5000/api` ;
 
 const api = axios.create({
   baseURL: E_RHM_API_URL,
